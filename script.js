5ca6c98541d50c1e20c9e203dea9cba27976676d
Jashwanth G <codesjash@gmail.com>
2026-03-09 18:41:49 +0530

    123

--- script.js
+++ script.js
@@ -1,30 +1,197 @@
-function generateLink(){
+const messageInput = document.getElementById("messageInput")
 
-let name=document.getElementById("name").value
-let msg=document.getElementById("message").value
+const fontSelect = document.getElementById("fontSelect")
 
-let encodedName=encodeURIComponent(name)
-let encodedMsg=encodeURIComponent(msg)
+const textColor = document.getElementById("textColor")
 
-let url=window.location.origin + "/wish.html?name=" + encodedName + "&msg=" + encodedMsg
+const fontSize = document.getElementById("fontSize")
 
-document.getElementById("link").innerHTML=
-"<a href='"+url+"' target='_blank'>"+url+"</a>"
+const applyTextBtn = document.getElementById("applyTextBtn")
+
+const textOverlay = document.getElementById("textOverlay")
+
+const templateItems = document.querySelectorAll(".templateItem")
+
+const templateImage = document.getElementById("templateImage")
+
+const stickers = document.querySelectorAll(".sticker")
+
+const stickersLayer = document.getElementById("stickersLayer")
+
+const saveBtn = document.getElementById("saveBtn")
+
+const shareBtn = document.getElementById("shareBtn")
+
+const recordBtn = document.getElementById("recordBtn")
+
+
+
+applyTextBtn.onclick = () =>
+{
+textOverlay.innerText = messageInput.value
+
+textOverlay.style.fontFamily = fontSelect.value
+
+textOverlay.style.color = textColor.value
+
+textOverlay.style.fontSize = fontSize.value + "px"
 }
 
-function getParams(){
 
-const params=new URLSearchParams(window.location.search)
 
-let name=params.get("name")
-let msg=params.get("msg")
+templateItems.forEach(template =>
+{
+template.onclick = () =>
+{
+templateImage.src = template.src
+}
+})
 
-if(name && msg){
 
-document.getElementById("wish").innerText=
-"🎂 Happy Birthday "+name+"! 🎉 "+msg
+
+stickers.forEach(sticker =>
+{
+
+sticker.onclick = () =>
+{
+
+const newSticker = document.createElement("div")
+
+newSticker.innerText = sticker.innerText
+
+newSticker.style.position = "absolute"
+
+newSticker.style.left = Math.random()*300 + "px"
+
+newSticker.style.top = Math.random()*300 + "px"
+
+newSticker.style.fontSize = "40px"
+
+newSticker.style.cursor = "move"
+
+stickersLayer.appendChild(newSticker)
+
+dragElement(newSticker)
+
+}
+
+})
+
+
+
+function dragElement(elmnt)
+{
+
+let pos1=0,pos2=0,pos3=0,pos4=0
+
+elmnt.onmousedown = dragMouseDown
+
+function dragMouseDown(e)
+{
+
+e = e || window.event
+
+e.preventDefault()
+
+pos3 = e.clientX
+
+pos4 = e.clientY
+
+document.onmouseup = closeDrag
+
+document.onmousemove = elementDrag
+
+}
+
+function elementDrag(e)
+{
+
+e = e || window.event
+
+e.preventDefault()
+
+pos1 = pos3 - e.clientX
+
+pos2 = pos4 - e.clientY
+
+pos3 = e.clientX
+
+pos4 = e.clientY
+
+elmnt.style.top = (elmnt.offsetTop - pos2) + "px"
+
+elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
+
+}
+
+function closeDrag()
+{
+document.onmouseup = null
+document.onmousemove = null
 }
 
 }
 
-window.onload=getParams
+
+
+dragElement(textOverlay)
+
+
+
+saveBtn.onclick = () =>
+{
+
+html2canvas(document.getElementById("cardCanvas")).then(canvas =>
+{
+
+let link = document.createElement("a")
+
+link.download = "birthday_wish.png"
+
+link.href = canvas.toDataURL()
+
+link.click()
+
+})
+
+}
+
+
+
+shareBtn.onclick = () =>
+{
+
+let text = messageInput.value
+
+let encoded = encodeURIComponent(text)
+
+let url = window.location.href + "?wish=" + encoded
+
+alert("Private Link:\n" + url)
+
+}
+
+
+
+window.onload = () =>
+{
+
+const params = new URLSearchParams(window.location.search)
+
+const wish = params.get("wish")
+
+if(wish)
+{
+textOverlay.innerText = decodeURIComponent(wish)
+}
+
+}
+
+
+
+recordBtn.onclick = () =>
+{
+
+alert("Video recording feature can capture animation in wearable devices.")
+
+}
